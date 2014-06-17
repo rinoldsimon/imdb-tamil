016f0088b82f0d494e68c83e0a29d02946e6afd2
rinoldsimon <crisrinold@gmail.com>
2014-06-17 22:49:23 +0530

    foundation:install and adding navbar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = 
+
+  config.assets.precompile += %w( vendor/modernizr.js )
 end
