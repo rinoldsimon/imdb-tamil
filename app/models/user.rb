7087dcf9f0233efe4a5936ba307c8fb0002cf9d3
rinoldsimon <crisrinold@gmail.com>
2014-06-26 23:41:31 +0530

    Google and Facebook login intergration using omniauth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,24 @@ class User < ActiveRecord::Base
   #devise :omniauthable, :omniauth_providers => [:facebook]
   #devise :omniauthable, :omniauth_providers => [:google_oauth2] 
 
-  def self.from_omniauth(auth)
-  where(auth.slice(:provider, :uid)).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-   # user.image = auth.info.image # assuming the user model has an image
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name:auth.extra.raw_info.name,
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20],
+                          )
+      end    
+    end
   end
-end
 
 
 def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
